239045f5602638b004ec9054a1bbb54453494b24
Pat Flannery <pat@flanneryonline.com>
2019-04-04 09:08:43 -0500

    initial commit

---
+++ dotnetcore-docker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcore_docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
